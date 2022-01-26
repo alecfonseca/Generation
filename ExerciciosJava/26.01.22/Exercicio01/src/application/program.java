f4194cb20877f82a1aa10146951d103a95830ece
unknown <alessandrocfonseca@hotmail.com>
2022-01-26 14:16:20 -0300

    ex01 java

---
+++ ExerciciosJava/26.01.22/Exercicio01/src/application/program.java
@@ -0,0 +1,36 @@
+package application;
+
+import java.util.Scanner;
+
+public class program {
+	public static void main(String[] args) {
+		Scanner leia = new Scanner(System.in);
+		
+		
+		System.out.print("Entre com a idade: ");
+		double anos = leia.nextDouble();
+		
+		System.out.print("Entre com os meses: ");
+		double meses = leia.nextDouble();
+		
+		System.out.print("Entre com os dias: ");
+	    double dias = leia.nextDouble();
+	    
+	    double total = 0;
+	    		total += dias;
+	    		total += (meses*30);
+	    		total += (anos*365);
+	    		
+	    		System.out.printf("O total em dias é: %.2f", total);
+	    		
+		
+		
+		
+		
+		
+		
+		leia.close();
+	
+	}
+
+}
